74ee1d143dbd0876c9b0c75d681fac4207615ae8
QuantumSoul <19613657+BinaryQuantumSoul@users.noreply.github.com>
2025-07-03 17:41:31 +0200

    move fonts

--- setup.sh
+++ setup.sh
@@ -19,10 +19,10 @@ ROFI_DIR="$HOME/.config/rofi"
 install_fonts() {
 	echo -e ${BBlue}"\n[*] Installing fonts..." ${Color_Off}
 	if [[ -d "$FONT_DIR" ]]; then
-		cp -rf $DIR/fonts/* "$FONT_DIR"
+		cp -rf $DIR/files/fonts/* "$FONT_DIR"
 	else
 		mkdir -p "$FONT_DIR"
-		cp -rf $DIR/fonts/* "$FONT_DIR"
+		cp -rf $DIR/files/fonts/* "$FONT_DIR"
 	fi
 	echo -e ${BYellow}"[*] Updating font cache...\n" ${Color_Off}
 	fc-cache
