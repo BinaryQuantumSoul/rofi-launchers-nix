ff2bc4c050ee26269742021b2a9fcffa7538dfb5
QuantumSoul <19613657+BinaryQuantumSoul@users.noreply.github.com>
2025-07-04 11:09:16 +0200

    add clipboard

---
+++ files/scripts/applets/bin/clipboard.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## Author  : QuantumSoul
+## Github  : @BinaryQuantumSoul
+#
+## Applets : Clipboard manager
+
+cliphist list | \
+rofi -dmenu \
+     -theme-str 'window {width: 50%;} listview {columns: 1;}'\
+     -theme-str "$ROFI_LAUNCH_THEME_COLOR_STR" -theme-str "$ROFI_LAUNCH_THEME_FONT_STR" \
+     -theme "$ROFI_LAUNCH_THEME_MAIN" \
+| cliphist decode | wl-copy
